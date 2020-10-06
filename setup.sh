2115e59d137d713596c15efb08f084c321515315
pafmon <pafmon@gmail.com>
2020-10-06 15:18:22 +0200

    Fix setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,37 @@
 
 
+# Download components
+echo "Downloading components"
+ 
 npx degit isa-group/governify-project-poetisa-evaluator evaluator
 npx degit isa-group/governify-project-poetisa-render render
 npx degit isa-group/governify-project-poetisa-billing billing
 npx degit isa-group/governify-project-poetisa-metrics metrics
 
-cd evaluator; npm install ; npm start & ; cd ..
-cd render; npm install ; npm start & ; cd ..
-cd metrics; npm install ; npm start & ; cd ..
-cd billing ; npm install ; npm start & ; cd ..
+# Setup / Run components
+ echo "Running evaluator component"
+ cd evaluator
+ npm install
+ node index &
+ cd ..
+ 
+ echo "Running render component"
+ cd render
+ npm install
+ node index & 
+ cd ..
 
+ echo "Running metrics component"
+ cd metrics
+ npm install
+ node index &
+ cd ..
+ 
+ echo "Running billing component"
+ cd billing
+ npm install
+ node index &
+ cd ..
+
+# Run InfluxDB
 docker run --name=influxdb -d -p 30086:8086 -e INFLUXDB_DB=k8s influxdb
